e90a2dfb0996a3aff11af5aecbf0310c3a17a997
Adam Miskiewicz <adam.skevy@mac.com>
2015-09-02 13:22:24 -0400

    allowFontScaling false

--- Libraries/Text/Text.js
+++ Libraries/Text/Text.js
@@ -113,9 +113,10 @@ var Text = React.createClass({
       isHighlighted: false,
     });
   },
+  
   getDefaultProps: function(): Object {
     return {
-      allowFontScaling: true,
+      allowFontScaling: false,
     };
   },
 

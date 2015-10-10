5a0d4d828571b9bb0ef71c9c6939b29abfeaacec
Brent Vatne <brentvatne@gmail.com>
2015-10-09 18:43:30 -0700

    Fix Flow error on StyleSheet.flatten
    # Conflicts:
    #	package.json

--- Libraries/StyleSheet/StyleSheet.js
+++ Libraries/StyleSheet/StyleSheet.js
@@ -62,6 +62,7 @@ var flattenStyle = require('flattenStyle');
 class StyleSheet {
   static flatten: typeof flattenStyle;
 
+
   static create(obj: {[key: string]: any}): {[key: string]: number} {
     var result = {};
     for (var key in obj) {
